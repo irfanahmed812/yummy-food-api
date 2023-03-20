d05df1a3c7a3feffe7f8f0cba5a156b3bdb19d4e
Irfan Ahmed <121118460+irfanahmed812@users.noreply.github.com>
2023-03-20 15:08:50 +0600

    Add files via upload

--- mealDb.js
+++ mealDb.js
@@ -21,7 +21,29 @@ const searchMeals = () => {
             .then(res => res.json())
             .then(data => displaySearchResult(data.meals))
     }
+
+    /* empty field */
+    emptyDisplayDetails()
+
+    /* searching is ...... */
+    const searching = document.getElementById('searching-id');
+    searching.innerHTML = `
+     <h5 class="fw-semibold">You're searching <span class="fw-bold"> "${searchText}"</span></h5>
+    `
 }
+
+/* empty so */
+
+/* Enter key function */
+
+const input = document.getElementById('input-field');
+
+input.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        searchMeals()
+    }
+})
+
 
 /* display search result */
 
@@ -47,7 +69,7 @@ const displaySearchResult = (meals) => {
             const div = document.createElement('div');
             div.classList.add('col')
             div.innerHTML = `
-                <div class="card">
+                <div class="card cursor" onclick="loadMealDetails(${meal?.idMeal})">
                     <img src="${meal?.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${meal?.strMeal.slice(0 - 31)}</h5>
@@ -55,9 +77,6 @@ const displaySearchResult = (meals) => {
                             <h6 class="card-title">Category: ${meal?.strCategory}</h6>
                             <h6 class="card-title">Area: ${meal?.strArea}</h6>
                             <h6 class="card-title">Tags: ${meal?.strTags}</h6>
-                            <div class="text-center pt-2">
-                                <button class="btn btn-outline-primary" onclick="loadMealDetails(${meal?.idMeal})">See more</button>
-                            </div>
                         </div>
                 </div>
         `
@@ -91,26 +110,48 @@ const displayMealDetails = (meal) => {
     mealDetails.textContent = '';
 
     const div = document.createElement('div')
-    div.classList.add('col')
+    div.classList.add('card')
     div.innerHTML = `
-        <div class="card">
-            <img src="${meal?.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${meal?.strMeal}</h5>
-                    <h6 class="card-title">ID: ${meal?.idMeal}</h6>
-                    <h6 class="card-title">Category: ${meal?.strCategory}</h6>
-                    <h6 class="card-title">Area: ${meal?.strArea}</h6>
-                    <h6 class="card-title">Tags: ${meal?.strTags}</h6>
-                    <p class="card-text"><h6>Discription:</h6>${meal?.strInstructions.slice(0 - 500)}</p>
-                    <div class="text-center">
-                        <h6 class="text-start">Recipe Video:</h6>
-                        <a href="${meal?.strYoutube}" target="_blank" class="text-black text-decoration-none fs-5 btn-light btn"><span class="text-danger"><i
-                        class="fa-brands fa-youtube"></i></span> Youtube</a>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm-4 mb-3 mb-sm-0">
+                        <div class="card-body">
+                            <div class="img-fluid meal-img">
+                                <img src="${meal?.strMealThumb}" alt="">
+                            </div>
                     </div>
                 </div>
-        </div>        
+                    <div class="col-sm-8">
+                        <div class="card-body">
+                            <h4 class="card-title">${meal?.strMeal}</h4>
+                            <h6 class="card-title">ID: ${meal?.idMeal}</h6>
+                            <h6 class="card-title">Category: ${meal?.strCategory}</h6>
+                            <h6 class="card-title">Area: ${meal?.strArea}</h6>
+                            <h6 class="card-title">Tags: ${meal?.strTags}</h6>
+                            <p class="card-text">
+                            <h6>Discription:</h6>${meal?.strInstructions.slice(0 - 500)}</p>
+                                <div class="text-center">
+                                    <h6 class="text-start">Recipe Video:</h6>
+                                    <a href="${meal?.strYoutube}" target="_blank" class="text-black text-decoration-none fs-5 btn-light btn"><span class="text-danger"><i class="fa-brands fa-youtube"></i></span> Youtube</a>
+                                    </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+
     `
 
     mealDetails.appendChild(div)
 }
 
+
+/* empty solve */
+const emptyDisplayDetails = () => {
+    const mealDetails = document.getElementById('meal-details');
+    const searchField = document.getElementById('input-field');
+    const searching = document.getElementById('searching-id');
+
+    searching.textContent = '';
+    mealDetails.textContent = '';
+    searchField.textContent = '';
+} 
